771e2cbbe3d88f05e62fdb85303c2a6b78bb22b4
ArnaudVanp <48100239+ArnaudVanp@users.noreply.github.com>
2019-05-15 18:23:00 +0200

    exChap3 update
    simplification du code - suppression des boucles for.

--- exoChap3/ex3.1.3.sol
+++ exoChap3/ex3.1.3.sol
@@ -2,50 +2,40 @@ pragma solidity ^0.5.7;
 
 contract Assemblee {
  
-    uint256 public membresCount = 0;
-    mapping(uint => Membres) public membres;
+    uint public totalMembres;
+    mapping(address => Membres) public membres;
     string[] descriptionDecisions;
     uint[] votesPour;
     uint[] votesContre;
-     
+    
     struct Membres {
-        uint _id;
-        address _adresseMembre;
+        bool estMembre;
+    }
+
+    function rejoindreAssemblee(address _adresseMembre) public {
+        require(estMembre(_adresseMembre) == false,"Cette adresse fait déjà partie des membres de l'assemblée");
+        totalMembres ++;
+        membres[_adresseMembre].estMembre = true;
     }
     
-    function rejoindreAssemblee(address _adresseMembre) public {
-      membresIncrement();
-      membres[membresCount] = Membres(membresCount, _adresseMembre);
-    }
-     
-    function membresIncrement() internal {
-          membresCount +=1;
-    }
-     
     
     function estMembre(address _participant) public view returns (bool) {
-         for (uint i=0; i <= membresCount; i++){
-             if(_participant == membres[i]._adresseMembre){
-                return true;
-             }
-         } return false;
+        return membres[_participant].estMembre;
     }
-     
     
     function proposerDecision(string memory _description) public {
-         if(estMembre(msg.sender))
-             {
-             descriptionDecisions.push(_description);
-             votesPour.push(0);
-             votesContre.push(0);
-            }
+         if(estMembre(msg.sender)) {
+            descriptionDecisions.push(_description);
+            votesPour.push(0);
+            votesContre.push(0);
+        }
     }
-     
+    
     //Définir la fonction voter, 
     //qui prend pour paramètres l’indice d’une proposition de décision et un entier pour déterminer 
     //le sens du vote (0 : contre, 1: pour) et incrémente le tableau votePour ou voteContre.
-    
-    function vote(uint _indiceVote, uint _sens) public {
+        
+    function voter(uint _indiceVote,uint _sens)public {
         if (_sens == 0) {
             votesContre[_indiceVote] = votesContre.push(1);
         } else if (_sens == 1) {
@@ -60,5 +50,5 @@ contract Assemblee {
     function comptabiliser(uint _indice) public view returns (int){
         return int(votesPour[_indice]-votesContre[_indice]); 
     }
-
+    
 }
