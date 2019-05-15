96d59ff89f0611b27fdf2484ba4d041a67e2b0fe
ArnaudVanp <48100239+ArnaudVanp@users.noreply.github.com>
2019-05-15 18:03:09 +0200

    exChap3 update
    simplification du code - suppression des boucles for.

--- exoChap3/ex3.1.2.sol
+++ exoChap3/ex3.1.2.sol
@@ -2,29 +2,20 @@ pragma solidity ^0.5.7;
 
 contract Assemblee {
  
- uint256 public membresCount = 0;
- mapping(uint => Membres) public membres;
- 
- struct Membres {
-    uint _id;
-    address _adresseMembre;
- }
+    uint public totalMembres;
+    mapping(address => Membres) public membres;
+    
+    struct Membres {
+        bool estMembre;
+    }
 
- function rejoindreAssemblee(address _adresseMembre) public {
-  membresIncrement();
-  membres[membresCount] = Membres(membresCount, _adresseMembre);
- }
- 
- function membresIncrement() public {
-      membresCount +=1;
- }
- 
- function estMembre(address _participant) public view returns (string memory) {
-     for (uint i=0; i <= membresCount; i++){
-         if(_participant == membres[i]._adresseMembre){
-            return "Le participant est bien membre de l'assemblée !";
-         }
-     } return "Le participant n'est pas membre de l'assemblée !";
- }
- 
+    function rejoindreAssemblee(address _adresseMembre) public {
+        require(estMembre(_adresseMembre) == false,"Cette adresse fait déjà partie des membres de l'assemblée");
+        totalMembres ++;
+        membres[_adresseMembre].estMembre = true;
+    }
+    
+    function estMembre(address _participant) public view returns (bool) {
+        return membres[_participant].estMembre;
+    } 
 }
